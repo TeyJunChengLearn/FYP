0080277ebed958f2a7a9b4fffa3bc675fa15765e
Elson Tey <elson@360.my>
2024-07-12 00:49:50 +0800

    add something

--- resources/views/forumPost.blade.php
+++ resources/views/forumPost.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.forumlayout')
 @section('css')
+<link rel="stylesheet" href="/css/test.css">
+<link rel="stylesheet" href="/css/nologin.css">
 @endsection
 @section('content')
 this is forum post page
